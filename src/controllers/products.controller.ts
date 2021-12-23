93fe341f2a8de6e224299c126236634483f636e1
izukuboi <isaacrm1395@gmail.com>
2021-12-22 21:21:09 -0400

    progress december 22

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -16,13 +16,10 @@ import { ProductsService } from 'src/services/products.service';
 export class ProductsController {
   constructor(private readonly productService: ProductsService) {}
   @Get()
-  getProducts(
-    @Query('limit') limit = 100,
-    @Query('offset') offset = 0,
-    @Query('brand') brand: string,
-  ): any {
+  getProducts(): any {
+    const products = this.productService.findAll();
     return {
-      message: `Products limit: ${limit} offset: ${offset} brand: ${brand} `,
+      products,
     };
   }
 
@@ -35,22 +32,21 @@ export class ProductsController {
   @HttpCode(HttpStatus.ACCEPTED)
   getProduct(@Param('productId') productId: number): any {
     let prod = this.productService.findOne(productId);
+    console.log(prod, 'lmao');
     return { prod };
   }
 
   @Post()
   create(@Body() payload: any) {
+    const createdProd = this.productService.create(payload);
     return {
       message: `created product`,
-      payload: payload,
+      createdProd,
     };
   }
   @Put(':productId')
   update(@Body() payload: any, @Param('productId') productId: string) {
-    return {
-      productId,
-      payload,
-    };
+    return this.productService.update(+productId, payload);
   }
   @Delete(':productId')
   delete(@Param('productId') productId: string) {
--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { find } from 'rxjs';
 import { product } from 'src/Entities/productEntity';
 
 @Injectable()
 export class ProductsService {
+  private counter: number = 2;
   private products: product[] = [
     {
       id: 1,
@@ -25,17 +27,27 @@ export class ProductsService {
     return this.products;
   }
   findOne(id: number) {
-    return this.products.find((prod) => prod.id === id);
+    let prod = this.products.find((prod) => prod.id == id);
+    return prod;
   }
   create(productToCreate: any) {
+    this.counter = this.counter + 1;
     const prodToCreate: product = {
-      description: productToCreate.decription,
-      id: this.products.pop().id + 1,
-      image: productToCreate.image,
-      name: productToCreate.name,
-      price: productToCreate.price,
-      stock: productToCreate.stock,
+      id: this.counter,
+      ...productToCreate,
     };
     return this.products.push(prodToCreate);
+  }
+  update(id: number, data: any) {
+    let prod = this.findOne(id);
+    if (prod != null) {
+      const index = this.products.findIndex((i) => i.id == id);
+      this.products[index] = {
+        ...prod,
+        ...data,
+      };
+      return this.products[index];
+    }
+    return null;
   }
 }
